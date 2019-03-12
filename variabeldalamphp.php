97d778874c531c0a09aca9f28f3df1af4c8d6a3f
danupras01 <danupras01@gmail.com>
2019-03-12 11:50:11 +0700

    variabel dalam php

---
+++ variabeldalamphp.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+		<title>Variabel dalam PHP</title>
+</head>
+<body>
+<?php
+//variabel bertipe integer
+$a="5";
+//variabel bertipe real
+$b="2.5";
+//variabel bertipe string
+$komentar="Selamat Datang di PHP";
+echo ("Nilai variabel a adalah = $a <br>"); 		//variabel bertipe integer
+echo ("Nilai variabel b adalah = $b <br>"); 		//variabel bertipe real
+echo ("Nilai variabel komentar adalah = $komentar<br>"); //variabel bertipe string
+$hasil=$a+$b;
+echo ("Hasil jumlah a dan b adalah = $hasil <br>");	//variabel bertipe double
+$tgl = date("d F Y");  					//variabel bertipe tanggal
+$nama = "Mabes TNI";
+$garis= "=====================================================";
+echo "<p>";
+echo $garis."<br>";
+echo $komentar. " Di kelas Pemrograman ". $nama. "<br>Belajar dengan giat ya.... <br>";
+echo $garis."<br>";
+echo "Tanggal ".$tgl;
+?>
+</body>
+</html>
